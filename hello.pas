ea874740b5cf020e60cca411c4417432f8525f52
Arsen Gadzhiyev <arsen.gadzhiyev.00@mail.ru>
2024-10-07 16:24:10 +0300

    Add choice of language

--- hello.pas
+++ hello.pas
@@ -1,3 +1,15 @@
 ﻿begin
-  Println('Привет, мир!');
+  Println('Specify the language you want (enter a number from 1 to 3)');
+  Println('1) China');
+  Println('2) Russian');
+  Println('3) English');
+  
+  var language := ReadInteger('Your choice:');  
+  Assert(language in 1..3);
+  
+  case language of
+    1: Println('你好，世界!');
+    2: Println('Привет, мир!');
+    3: Println('Hello, world!');
+  end;
 end.
